3ba55cf2c3761018989df9d8283c8b6eaa42a1d1
theo-baccam <theo.baccam@laplateforme.io>
2024-06-10 16:36:44 +0200

    jour01/job09: different ways of initializing a string

---
+++ jour01/job09/MyString.java
@@ -0,0 +1,13 @@
+public class MyString {
+    public static void main(String[] arg) {
+        String first = "First";
+
+        String second;
+        second = "Second";
+
+        String third = new String("Third");
+
+        char[] fourthCharArray = {'f', 'o', 'u', 'r', 't', 'h'};
+        String fourthString = new String(fourthCharArray);
+    }
+}
