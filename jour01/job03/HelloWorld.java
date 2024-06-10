b4618a3250b10c1923a19664ae1d60ff12d7e225
theo-baccam <theo.baccam@laplateforme.io>
2024-06-10 15:07:26 +0200

    jour01/job03: comments

---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    // 'public' means any object can access it.
+
+    // 'static' means that this method is associated with the class, and not
+    // a specific instance of that class
+
+    // 'void' means it returns nothing
+
+    public static void main(String[] args) { // 'String[] args' -> CLI arguments
+        System.out.println("Hello World");
+    }
+}
