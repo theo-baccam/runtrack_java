c376f87d3f2386ed4f7101698f23685bf2ee77db
theo-baccam <theo.baccam@laplateforme.io>
2024-06-11 10:35:30 +0200

    jour02/job09: récupérer le premier et dernier chiffres du nombre 1234

--- jour02/job09/Job09.java
+++ jour02/job09/Job09.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
-
 public class Job09 {
     public static void main(String[] arg) {
-        Scanner inputScanner = new Scanner(System.in);
-        
-        System.out.println("Give an integer:");
-        int number = inputScanner.nextInt();
+        int number = 1234;
 
         int firstDigit = number;
         int tmp = number;
