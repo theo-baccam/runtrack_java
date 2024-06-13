58833ae38a6770d3f56691a55fe71f4ec97a7a45
theo-baccam <theo.baccam@laplateforme.io>
2024-06-13 09:48:49 +0200

    jour04/job01: création d'un thread

---
+++ jour04/job01/MyThread.java
@@ -0,0 +1,8 @@
+package job01;
+
+
+public class MyThread extends Thread {
+    public void run() {
+        System.out.println("Mon thread est en cours");
+    }
+}
---
+++ jour04/job01/ThreadProject.java
@@ -0,0 +1,9 @@
+package job01;
+
+
+public class ThreadProject {
+    public static void main(String[] arg) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+}
