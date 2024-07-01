adbcb6b0c25d9b26c3492a7bd672c0f1582adde7
theo-baccam <theo.baccam@laplateforme.io>
2024-07-01 11:06:51 +0200

    jour06/job01: récupérer et afficher une saisie clavier

---
+++ jour06/job01/Job01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Job01 {
+    public static void main(String[] arg) {
+        Scanner inputScanner = new Scanner(System.in);
+        String userInput = inputScanner.nextLine();
+        System.out.println(userInput);
+    }
+}
