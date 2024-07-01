2761e2fc1d03435e8bb07997d0c3753a62a71a93
theo-baccam <theo.baccam@laplateforme.io>
2024-07-01 11:11:14 +0200

    jour06/job02: Demander le prénom d'un utilisateur et l'afficher

---
+++ jour06/job02/Job02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class Job02 {
+    public static void main(String[] arg) {
+        Scanner inputScanner = new Scanner(System.in);
+        String firstName = inputScanner.nextLine();
+        System.out.printf("bonjour %s%n", firstName);
+    }
+}
